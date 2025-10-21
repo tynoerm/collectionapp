49a3b6c6873758c9d1dd47940a1ae6b9336bcfc5
tynoerm <tinomutendaishemutemaringa@gmail.com>
2025-10-21 14:40:39 +0200

    Change MongoDB connection password
    Updated MongoDB connection string to use a new password.

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 // ✅ MongoDB connection
 mongoose
-  .connect('mongodb+srv://tinomutendaishemutemaringa:Tinomutenda123#@cluster0.1cmpp1i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  .connect('mongodb+srv://tinomutendaishemutemaringa:XZRubSiUTEPrRMem@cluster0.1cmpp1i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -46,4 +46,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
+
 
